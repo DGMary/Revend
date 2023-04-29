789b2e75fceb66b276c148f2da19b1296556f1a5
mariia.mynkina <mariia.mynkina@danads.se>
2023-04-29 16:48:06 +0300

    Fix mobile header.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
-const btnBurger = document.querySelector('.burger-button');
+const btnBurger = document.querySelector('.js-open-menu');
+const btnBurgerClose = document.querySelector('.js-close-menu');
 const mainBody = document.querySelector('.body');
+const topPanelBtn = document.querySelector('.js-top-panel');
+const topPanel = document.querySelector('.top-panel');
 
 btnBurger.addEventListener('click', function() {
-  mainBody.classList.toggle('opened-menu');
+  mainBody.classList.add('opened-menu');
+});
+btnBurgerClose.addEventListener('click', function() {
+  mainBody.classList.remove('opened-menu');
+});
+topPanelBtn.addEventListener('click', function() {
+  topPanel.classList.remove('is-active');
 });
