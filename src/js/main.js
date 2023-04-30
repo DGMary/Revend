014bca6a363c13340e0bcc79db77e850bc17b6c4
mariia.mynkina <mariia.mynkina@danads.se>
2023-04-30 17:06:26 +0300

    Fix scripts.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,30 @@
-const btnBurger = document.querySelector('.js-open-menu');
-const btnBurgerClose = document.querySelector('.js-close-menu');
-const mainBody = document.querySelector('.body');
-const topPanelBtn = document.querySelector('.js-top-panel');
-const topPanel = document.querySelector('.top-panel');
+window.addEventListener("DOMContentLoaded", (event) => {
+  const btnBurger = document.querySelector('.js-open-menu');
+  const btnBurgerClose = document.querySelector('.js-close-menu');
+  const mainBody = document.querySelector('.body');
+  const topPanelBtn = document.querySelector('.js-top-panel');
+  const topPanel = document.querySelector('.top-panel');
+  const infoCloseBtn = document.querySelector('.js-close-info');
+  const infoPopup = document.querySelector('.js-info-block');
 
-btnBurger.addEventListener('click', function() {
-  mainBody.classList.add('opened-menu');
-});
-btnBurgerClose.addEventListener('click', function() {
-  mainBody.classList.remove('opened-menu');
-});
-topPanelBtn.addEventListener('click', function() {
-  topPanel.classList.remove('is-active');
+  if (btnBurger) {
+    btnBurger.addEventListener('click', function() {
+      mainBody.classList.add('opened-menu');
+    });
+  }
+  if (btnBurgerClose) {
+    btnBurgerClose.addEventListener('click', function() {
+      mainBody.classList.remove('opened-menu');
+    });
+  }
+  if (topPanelBtn) {
+    topPanelBtn.addEventListener('click', function() {
+      topPanel.classList.remove('is-active');
+    });
+  }
+
+  infoCloseBtn.addEventListener('click', function() {
+    infoPopup.classList.remove('is-active');
+  });
+
 });
